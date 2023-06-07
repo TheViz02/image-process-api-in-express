38ff3ef191bbda7c9dd2738140f22b07b1aa9c06
Bhupendra Madhu <68414570+TheViz02@users.noreply.github.com>
2023-06-07 20:45:24 +0530

    Update script.js
    Error handling for JS script

--- script.js
+++ script.js
@@ -5,28 +5,6 @@ $("span#text-response").hide();
  */
 
 const API_URL = "http://localhost:3001/api/imgProcess";
-/* $("form#imageUpload").on("submit", function (event) {
-
-  event.preventDefault();
-
-  $("div#spinner").show();
-  // const form = document.getElementById("imageUpload");
-  let imageFile = document.getElementById("uploadImage");
-  let fileReader = imageFile.files[0];
-  let data = new FormData();
-
-  data.append("image", fileReader);
-  console.log(data);
-
-  $.ajax({
-    url: API_URL,
-    data: data,
-    dataType: "multipart/form-data",
-    method: "POST",
-  }).done(function (resp) {
-    console.log(resp);
-  });
-}); */
 
 $("form#imageUpload").on("submit", function (event) {
   event.preventDefault();
@@ -53,6 +31,9 @@ $("form#imageUpload").on("submit", function (event) {
       $("span#text-response").html(resp.processResp);
     })
     .fail(function (error) {
-      console.error("Error uploading image:", error);
+      console.error(error);
+      $("div#spinner").hide();
+      $("span#text-response").show();
+      $("span#text-response").html("An Error Occur, please check");
     });
 });
